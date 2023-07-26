4fd012f37c71bb5543fdd75a2015a89f7c065906
Mariia Lavrenchuk <marymyyy@gmail.com>
2023-07-26 15:58:38 +0200

    App for accounting of employees

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = ({employees, increased}) => {
+    return (
+        <div className="app-info">
+            <h1>Облік співробітників у компанії N</h1>
+            <h2>Загальна кількість працівників: {employees}</h2>
+            <h2>Премію отримають: {increased}</h2>
+        </div>
+    )
+}
+
+export default AppInfo;
